e1e7976cb4e0b41b92d361ee33d65ebf3d42c861
Juyoung Shim <70943528+angela-sjy@users.noreply.github.com>
2022-11-13 22:16:45 +0900

    first commit

---
+++ team16/LandinPage.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Nice PHP Page</title>
+</head>
+<body>
+    <h1>
+        <?php 
+        echo 'Hello My Friend!<br/>';
+        echo '웹서버 테스트<br/>';
+        echo 'php 실행 테스트'; 
+        ?>
+    </h1>
+    <p>I am PHP!</p>
+</body>
+</html>
+
